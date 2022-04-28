afea34c4bf583136bebf8ddd52d9cddae0e5b12f
Minjun <84955607+devkr-potato@users.noreply.github.com>
2022-04-28 20:53:45 +0900

    Add files via upload

---
+++ design/src/script.js
@@ -0,0 +1,7 @@
+document.querySelector('#profileMore').addEventListener('click',()=>{
+  document.querySelector('.modal').style.display = "block";
+});
+
+document.querySelector('#closeModal').addEventListener('click',()=>{
+  document.querySelector('.modal').style.display = "none";
+});
