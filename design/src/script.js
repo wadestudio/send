977005d6378cbc4ce67c4d34a435bd0d6859809e
Minjun <84955607+dev-yuka05@users.noreply.github.com>
2022-07-17 16:05:18 +0900

    Delete design directory

--- design/src/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('#profileMore').addEventListener('click',()=>{
-  document.querySelector('.modal').style.display = "block";
-});
-
-document.querySelector('#closeModal').addEventListener('click',()=>{
-  document.querySelector('.modal').style.display = "none";
-});
